60ec15f81c2337a31063d71b0f190089770b1a47
sinkaroid <bibsindra@gmail.com>
2022-04-10 11:24:05 +0700

    test: change limit value to 100

--- test/scathach.search.ts
+++ test/scathach.search.ts
@@ -2,14 +2,14 @@ import Scathach from '../build/index';
 
 const sca = new Scathach();
 
-sca.searchGelbooru('cat_girl', 25).then((r) => {
+sca.searchGelbooru('cat_girl', 100).then((r) => {
   console.log('cat_girl', r);
 });
 
-sca.searchR34('futanari', 25).then((r) => {
+sca.searchR34('futanari', 100).then((r) => {
   console.log('futanari', r);
 });
 
-sca.searchSafe('elf', 25).then((r) => {
+sca.searchSafe('elf', 100).then((r) => {
   console.log('elf', r);
 });
