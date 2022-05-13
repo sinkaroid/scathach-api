4866488b398f0dff8df80145e7e57564831e9bb7
sinkaroid <bibsindra@gmail.com>
2022-05-13 13:47:37 +0700

    feat: add new methods and proper error blocks

--- index.ts
+++ index.ts
@@ -18,54 +18,51 @@ class Scathach {
 
 
   /**
-   * Get a Random Normalizes FGO lewds data
+   * Get a random normalizes FGO sex data
    * @returns Unfiltered <code>scathach.redsplit.org/v5/fgo</code> Response
    */
   async getFgo() {
-    try {
-      const res = await request({
-        category: 'fgo',
-        useragent: this.useragent,
-        apikey: this.apikey
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+
+    const res = await request({
+      category: 'fgo',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+    
+    return res;
+
   }
 
   /**
-   * Get a Random Normalizes Genshin lewds data
+   * Get a random normalizes Genshin impact sex data
    * @returns Unfiltered <code>scathach.redsplit.org/v5/genshin</code> Response
    */
   async getGenshin() {
-    try {
-      const res = await request({
-        category: 'genshin',
-        useragent: this.useragent,
-        apikey: this.apikey
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+
+    const res = await request({
+      category: 'genshin',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
   /**
-  * Get a Random Normalizes Azurlane sex data
+  * Get a random normalizes Azur lane sex data
   * @returns Unfiltered <code>scathach.redsplit.org/v5/azur_lane</code> Response
   */
   async getAzur() {
-    try {
-      const res = await request({
-        category: 'azur',
-        useragent: this.useragent,
-        apikey: this.apikey
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+
+    const res = await request({
+      category: 'azur',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
   /**
@@ -73,39 +70,115 @@ class Scathach {
    * @returns Unfiltered scathach.redsplit.org/v5/waifu Response
    */
   async getWaifu() {
-    try {
-      const res = await request({
-        category: 'waifu',
-        useragent: this.useragent,
-        apikey: this.apikey
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+
+    const res = await request({
+      category: 'waifu',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
   /**
-   * Get a random sex porn gif
+  * Get a random normalizes Arknights sex data
+  * @returns Unfiltered <code>scathach.redsplit.org/v5/arknights</code> Response
+  */
+  async getArknights() {
+    const res = await request({
+      category: 'arknights',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
+  }
+
+  /**
+  * Get a random normalizes Fire emblem sex data
+  * @returns Unfiltered <code>scathach.redsplit.org/v5/fire_emblem</code> Response
+  */
+  async getFireEmblem() {
+
+    const res = await request({
+      category: 'fire_emblem',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
+  }
+
+  /**
+  * Get a random normalizes Girls' frontline sex data
+  * @returns Unfiltered <code>scathach.redsplit.org/v5/gfl</code> Response
+  */
+  async getGirlsFrontline() {
+
+    const res = await request({
+      category: 'gfl',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
+  }
+
+  /**
+  * Get a random normalizes Hololive vtuber sex data
+  * @returns Unfiltered <code>scathach.redsplit.org/v5/hololive</code> Response
+  */
+  async getHololive() {
+
+    const res = await request({
+      category: 'hololive',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
+  }
+
+  /**
+  * Get a random normalizes Kancolle girls sex data
+  * @returns Unfiltered <code>scathach.redsplit.org/v5/kancolle</code> Response
+  */
+  async getKancolle() {
+ 
+    const res = await request({
+      category: 'kancolle',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
+  }
+
+  /**
+   * Get a random sex porn gif real shit
    * @returns Unfiltered scathach.redsplit.org/v5/sex Response
    */
   async getSex() {
-    try {
-      const res = await request({
-        category: 'sex',
-        useragent: this.useragent,
-        apikey: this.apikey
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+
+    const res = await request({
+      category: 'sex',
+      useragent: this.useragent,
+      apikey: this.apikey
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
   /**
    * Get a reaction from servant fate
    * @param img The reaction to get
-   * @returns Unfiltered servant.gitlab.com/d0g/servant/-/raw/master/_constant/ Response
    * @example
    * getReaction("angry")
    * getReaction("bonk")
@@ -123,16 +196,62 @@ class Scathach {
    * @returns the random reaction that endpoints given
    */
   async getReaction(img: string) {
-    try {
-      const res = await request({
-        category: 'reaction',
-        img,
-        useragent: this.useragent
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+ 
+    const res = await request({
+      category: 'reaction',
+      img,
+      useragent: this.useragent
+    }).catch((err) => console.error(err));
+
+    return res;
+
+  }
+
+  /**
+   * Get random anime sex
+   * @param img The endpoints to get
+   * @example
+   * getAnimeSex("ass")
+   * getAnimeSex("bdsm")
+   * getAnimeSex("cum")
+   * getAnimeSex("creampie")
+   * getAnimeSex("manga")
+   * getAnimeSex("femdom")
+   * getAnimeSex("hentai")
+   * getAnimeSex("incest")
+   * getAnimeSex("masturbation")
+   * getAnimeSex("public")
+   * getAnimeSex("ero")
+   * getAnimeSex("orgy")
+   * getAnimeSex("elves")
+   * getAnimeSex("yuri")
+   * getAnimeSex("pantsu")
+   * getAnimeSex("glasses")
+   * getAnimeSex("blowjob")
+   * getAnimeSex("boobjob")
+   * getAnimeSex("foot")
+   * getAnimeSex("thighs")
+   * getAnimeSex("vagina")
+   * getAnimeSex("ahegao")
+   * getAnimeSex("uniform")
+   * getAnimeSex("gangbang")
+   * getAnimeSex("tentacles")
+   * getAnimeSex("gif")
+   * getAnimeSex("neko")
+   * getAnimeSex("nsfwMobileWallpaper")
+   * getAnimeSex("zettaiRyouiki")
+   * @returns the random anime sex from endpoints given
+   */
+  async getAnimeSex(img: string) {
+
+    const res = await request({
+      category: 'animesex',
+      img,
+      useragent: this.useragent
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
   /**
@@ -147,16 +266,14 @@ class Scathach {
    * @returns A data that represents the proxied gelbooru response from scathach bot
    */
   async searchGelbooru(tags: string | string[], limit?: number, page?: number) {
-    try {
-      const res = await request({
-        category: 'gelbooru',
-        limit, tags, page,
-        useragent: this.useragent
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+    const res = await request({
+      category: 'gelbooru',
+      limit, tags, page,
+      useragent: this.useragent
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
   /**
@@ -171,16 +288,15 @@ class Scathach {
    * @returns A data that represents the proxied r34 response from scathach bot
    */
   async searchR34(tags: string | string[], limit?: number, page?: number) {
-    try {
-      const res = await request({
-        category: 'r34',
-        limit, tags, page,
-        useragent: this.useragent
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+ 
+    const res = await request({
+      category: 'r34',
+      limit, tags, page,
+      useragent: this.useragent
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
   /**
@@ -195,16 +311,15 @@ class Scathach {
    * @returns A data that represents the proxied safebooru response from scathach bot
    */
   async searchSafe(tags: string | string[], limit?: number, page?: number) {
-    try {
-      const res = await request({
-        category: 'safe',
-        limit, tags, page,
-        useragent: this.useragent
-      });
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
+
+    const res = await request({
+      category: 'safe',
+      limit, tags, page,
+      useragent: this.useragent
+    }).catch((err) => console.error(err));
+
+    return res;
+
   }
 
 }
